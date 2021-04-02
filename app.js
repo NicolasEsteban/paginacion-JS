f1d1d1c8e81dd563d6203d8b9e9064971b03ad50
nicolas <nico.92muz@gmail.com>
2021-04-01 21:07:04 -0300

    Se modifica cantidad de botones paginacion

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var ranks = [];
 // Funciones
 // Se inicia el contenido de prueba
 function startParameters() {
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 100; i++) {
         data.push({ id: i + 1, name: 'usuario ' + (i + 1) });
 
     }
@@ -56,11 +56,11 @@ function calculateButtons(ranks){
     var numbers = [];
     if(pageNumber<ranks[0]){
         ranks[0]= pageNumber;
-        ranks[1]= pageNumber + 2;
+        ranks[1]= pageNumber + 4;
     }
     if(pageNumber>ranks[1]){
         ranks[1] = pageNumber;
-        ranks[0] = pageNumber - 2;
+        ranks[0] = pageNumber - 4;
     }
 
     console.log(pageNumber);
@@ -85,14 +85,14 @@ function selectPageSize(){
 // Funcion principal que realiza la paginacion y se escriben los botones en la vista
 function showData(_data) {
     var pagination = paginate(data, pageSize, pageNumber);
-    //cantidad de pagina es mayor a 3
+    //cantidad de pagina es mayor a 5
     
     
     writeBodyTable(pagination);
     html = `<div class="btn-group me-2" role="group" aria-label="First group"></div>`;
     html += pageNumber >1  ? `<button type="button" id="previus" class="btn btn-outline-primary" onclick='previusPage()' >  &laquo; </button> `: ``;
-    //Si la pcantidad de pagina es mayor que 3, se imprimiran los botones del medio
-    if (pageCont > 3) { 
+    //Si la pcantidad de pagina es mayor que 5, se imprimiran los botones del medio
+    if (pageCont > 5) { 
         var numberButtons = calculateButtons(ranks);
         console.log('numero botones: '+numberButtons);
         numberButtons.forEach(numberB =>{
@@ -124,7 +124,7 @@ pageNumber = 1;
 // Elementos por pagina
 pageSize = 6;
 // Elemento utilizados para obtener los botones que estaran al medio 
-ranks.push(pageNumber,pageNumber+2);
+ranks.push(pageNumber,pageNumber+4);
 html = "";
 // Cantidad de paginas
 pageCont = Math.ceil(data.length / pageSize);
